12fc9357075b9e95b06ca81f66f600568838041d
Nhat Ha <nhathadt11@gmail.com>
2018-03-04 01:12:27 +0700

    Add servlet dependency package

---
+++ src/main/java/com/prm/controllers/FrontController.java
@@ -0,0 +1,6 @@
+package com.prm.controllers;
+
+@javax.servlet.annotation.WebServlet(name = "FrontController", value = {"/front-controller"})
+public class FrontController {
+// TODO: 04/03/2018 Implement this as a dispatching controller
+}
